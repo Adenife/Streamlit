6cdd12e5a933a902587fe6aad0c42c4bc9d83104
Aweda Oluwanifemi Adeola <41619073+Adenife@users.noreply.github.com>
2022-03-15 19:27:20 +0100

    markdown edit

--- EDA-Application/app.py
+++ EDA-Application/app.py
@@ -29,9 +29,14 @@ if uploaded_file is not None:
         csv = pd.read_csv(uploaded_file)
         return csv
     df = load_csv()
+
     pr = ProfileReport(df, explorative=True)
     st.header('**Input DataFrame**')
+    st.markdown("""
+    Dataframe table at a glance
+    """)
     st.write(df)
+    
     st.write('---')
     st.header('**Pandas Profiling Report**')
     st_profile_report(pr)
