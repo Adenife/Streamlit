180289c7f71a7a44bf1e11dcaf96944b8d0f9e5e
Aweda Oluwanifemi Adeola <41619073+Adenife@users.noreply.github.com>
2022-07-26 14:32:09 +0100

    Improved the stock price application

--- StockPrice-Application/app.py
+++ StockPrice-Application/app.py
@@ -8,6 +8,8 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 
+st.set_page_config(page_title="Stock App", page_icon=":shark:", layout="wide", initial_sidebar_state="collapsed")
+
 # Title
 st.markdown('''
 # Stock Price Tracker
@@ -31,163 +33,88 @@ st.sidebar.markdown("""
 This application is built using Python and Streamlit by: 
 [Aweda Oluwanifemi](https://www.linkedin.com/in/oluwanifemi-aweda-2b9206118/)
 
-\nTutorials from [Chanin Nantasenamat](https://medium.com/@chanin.nantasenamat) (aka [Data Professor](http://youtube.com/dataprofessor))
+\nIdea from [Data Professor](http://youtube.com/dataprofessor)
 """)
 
 
-
+st.title('Company Profile')
 company_logo = '<img src=%s>' % tickerData.info['logo_url']
 st.markdown(company_logo, unsafe_allow_html=True)
+st.subheader(tickerData.info['longName']) 
 
-company_name = tickerData.info['longName']
-st.header('**%s**' % company_name)
 
-company_employees = tickerData.info['fullTimeEmployees']
-st.header('Fulltime employees: %s' % company_employees)
+col1, col2 = st.columns(2)
+with col1:
+    st.markdown('**Industry**: ' + tickerData.info['industry'])
+    st.markdown('**Address**: ' + tickerData.info['address1'] + ', ' + tickerData.info['city'] + ', ' + tickerData.info['zip'] + ', '  +  tickerData.info['country'])
+    st.markdown('**Number of Employees**: %s' % tickerData.info['fullTimeEmployees'])
+with col2:
+    st.markdown('**Sector**: ' + tickerData.info['sector'])
+    st.markdown('**Phone**: ' + tickerData.info['phone'])
+    st.markdown('**Website**: ' + tickerData.info['website'])
 
-company_summary = tickerData.info['longBusinessSummary']
-st.info(company_summary)
+st.markdown('**Business Summary**')
+st.info(tickerData.info['longBusinessSummary'])
+
+
+
+fundInfo1 = {
+    'Enterprise Value (USD)': tickerData.info['enterpriseValue'],
+    'Enterprise To Revenue Ratio': tickerData.info['enterpriseToRevenue'],
+    'Enterprise To Ebitda Ratio': tickerData.info['enterpriseToEbitda'],
+    'Net Income (USD)': tickerData.info['netIncomeToCommon'],
+    'Profit Margin Ratio': tickerData.info['profitMargins'],
+    'Forward PE Ratio': tickerData.info['forwardPE'],
+}
+
+fundInfo2 = {
+    'PEG Ratio': tickerData.info['pegRatio'],
+    'Price to Book Ratio': tickerData.info['priceToBook'],
+    'Forward EPS (USD)': tickerData.info['forwardEps'],
+    'Beta ': tickerData.info['beta'],
+    'Book Value (USD)': tickerData.info['bookValue'],
+    'Dividend Rate (%)': tickerData.info['dividendRate'], 
+    'Dividend Yield (%)': tickerData.info['dividendYield'],
+    'Five year Avg Dividend Yield (%)': tickerData.info['fiveYearAvgDividendYield'],
+    'Payout Ratio': tickerData.info['payoutRatio']
+}
+
+col3, col4 = st.columns(2)
+
+with col3:
+    fundDF1 = pd.DataFrame.from_dict(fundInfo1, orient='index')
+    fundDF1 = fundDF1.rename(columns={0: 'Value'})
+    st.subheader('Fundamental Information') 
+    st.table(fundDF1)
+
+with col4:
+    fundDF2 = pd.DataFrame.from_dict(fundInfo2, orient='index')
+    fundDF2 = fundDF2.rename(columns={0: 'Value'})
+    st.table(fundDF2)
 
 
 #  Bollinger bands
-st.header('**Market Trends**')
-qf=cf.QuantFig(tickerDf,title='Market Chart',legend='top',name='GS',up_color='green', down_color='red')
+st.header('**Bollinger Bands**')
+qf=cf.QuantFig(tickerDf,title='Market Trend',legend='top',name='GS',up_color='green', down_color='red')
 qf.add_bollinger_bands(periods=20, boll_std=2, colors=['cyan','grey'], fill=True,)
 qf.add_volume(name='Volume',up_color='green', down_color='red')
 fig = qf.iplot(asFigure=True)
-st.plotly_chart(fig)
+st.plotly_chart(fig, use_container_width=True)
 
 
-# # Candlestick plot
-# prices = tickerDf['Close']
-# volumes = tickerDf['Volume']
+marketInfo = {
+    "Volume": tickerData.info['volume'],
+    "Average Volume": tickerData.info['averageVolume'],
+    "Market Cap": tickerData.info["marketCap"],
+    "Float Shares": tickerData.info['floatShares'],
+    "Regular Market Price (USD)": tickerData.info['regularMarketPrice'],
+    'Bid Size': tickerData.info['bidSize'],
+    'Ask Size': tickerData.info['askSize'],
+    "Share Short": tickerData.info['sharesShort'],
+    'Short Ratio': tickerData.info['shortRatio'],
+    'Share Outstanding': tickerData.info['sharesOutstanding']
+}
 
-# lower = prices.min()
-# upper = prices.max()
-# prices_ax = np.linspace(lower,upper, num=20)
-
-# vol_ax = np.zeros(20)
-
-
-# # Accumulate volume for each price points (Create bins)
-# for i in range(0, len(volumes)):
-#     if(prices[i] >= prices_ax[0] and prices[i] < prices_ax[1]):
-#         vol_ax[0] += volumes[i]   
-        
-#     elif(prices[i] >= prices_ax[1] and prices[i] < prices_ax[2]):
-#         vol_ax[1] += volumes[i]  
-        
-#     elif(prices[i] >= prices_ax[2] and prices[i] < prices_ax[3]):
-#         vol_ax[2] += volumes[i] 
-        
-#     elif(prices[i] >= prices_ax[3] and prices[i] < prices_ax[4]):
-#         vol_ax[3] += volumes[i]  
-        
-#     elif(prices[i] >= prices_ax[4] and prices[i] < prices_ax[5]):
-#         vol_ax[4] += volumes[i]  
-        
-#     elif(prices[i] >= prices_ax[5] and prices[i] < prices_ax[6]):
-#         vol_ax[5] += volumes[i] 
-        
-#     elif(prices[i] >= prices_ax[6] and prices[i] < prices_ax[7]):
-#         vol_ax[6] += volumes[i] 
-
-#     elif(prices[i] >= prices_ax[7] and prices[i] < prices_ax[8]):
-#         vol_ax[7] += volumes[i] 
-
-#     elif(prices[i] >= prices_ax[8] and prices[i] < prices_ax[9]):
-#         vol_ax[8] += volumes[i] 
-
-#     elif(prices[i] >= prices_ax[9] and prices[i] < prices_ax[10]):
-#         vol_ax[9] += volumes[i] 
-
-#     elif(prices[i] >= prices_ax[10] and prices[i] < prices_ax[11]):
-#         vol_ax[10] += volumes[i] 
-
-#     elif(prices[i] >= prices_ax[11] and prices[i] < prices_ax[12]):
-#         vol_ax[11] += volumes[i] 
-
-#     elif(prices[i] >= prices_ax[12] and prices[i] < prices_ax[13]):
-#         vol_ax[12] += volumes[i] 
-
-#     elif(prices[i] >= prices_ax[13] and prices[i] < prices_ax[14]):
-#         vol_ax[13] += volumes[i] 
-
-#     elif(prices[i] >= prices_ax[14] and prices[i] < prices_ax[15]):
-#         vol_ax[14] += volumes[i]   
-        
-#     elif(prices[i] >= prices_ax[15] and prices[i] < prices_ax[16]):
-#         vol_ax[15] += volumes[i] 
-        
-#     elif(prices[i] >= prices_ax[16] and prices[i] < prices_ax[17]):
-#         vol_ax[16] += volumes[i]         
-        
-#     elif(prices[i] >= prices_ax[17] and prices[i] < prices_ax[18]):
-#         vol_ax[17] += volumes[i]         
-        
-#     elif(prices[i] >= prices_ax[18] and prices[i] < prices_ax[19]):
-#         vol_ax[18] += volumes[i] 
-    
-#     else:
-#         vol_ax[19] += volumes[i]
-
-
-# # make plots
-# fig = make_subplots(
-#         rows=1, cols=2,
-#         column_widths=[0.2, 0.8],
-#         specs=[[{}, {}]],
-#         horizontal_spacing = 0.01
-#     )
-
-# fig.add_trace(
-#         go.Bar(
-#                 x = vol_ax, 
-#                 y= prices_ax,
-#                 text = np.around(prices_ax,2),
-#                 textposition='auto',
-#                 orientation = 'h'
-#             ),
-#         row = 1, col =1
-#     )
-
-# dateStr = tickerDf.index.strftime("%d-%m-%Y %H:%M:%S")
-
-# fig.add_trace(
-#     go.Candlestick(x=dateStr,
-#                 open=tickerDf['Open'],
-#                 high=tickerDf['High'],
-#                 low=tickerDf['Low'],
-#                 close=tickerDf['Close'],
-#                 yaxis= "y2"  
-#             ),
-#         row = 1, col=2
-#     )
-        
-# fig.update_layout(
-#     title_text='Market Profile Chart (US S&P 500)', # title of plot
-#     bargap=0.01, # gap between bars of adjacent location coordinates,
-#     showlegend=False,
-    
-#     xaxis = dict(
-#             showticklabels = False
-#         ),
-#     yaxis = dict(
-#             showticklabels = False
-#         ),
-    
-#     yaxis2 = dict(
-#             title = "Price (USD)",
-#             side="right"
-#         )
-# )
-
-# fig.update_yaxes(nticks=20)
-# fig.update_yaxes(side="right")
-# fig.update_layout(height=800)
-
-# config={
-#         'modeBarButtonsToAdd': ['drawline']
-#     }
-
-# st.plotly_chart(fig, use_container_width=True, config=config)
+marketDF = pd.DataFrame(data=marketInfo, index=[0])
+st.subheader('Stock Market Details')
+st.table(marketDF)
